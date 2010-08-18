9aac3d92ca0def254c21272d61b46b75c690e5c6
Jeremy Carbaugh <jcarbaugh@gmail.com>
2010-08-18 12:42:08 -0400

    add host to settings

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ from cupcakes import app, settings
 if __name__ == "__main__":
     
     port = 8000 if settings.DEBUG else 80
+    host = 'localhost' if not hasattr(settings, 'HOST') else settings.HOST
 
     app.secret_key = settings.SECRET_KEY or "thisreallyisntasecretkey"
-    app.run(port=port, debug=settings.DEBUG)
+    app.run(host=host, port=port, debug=settings.DEBUG)
